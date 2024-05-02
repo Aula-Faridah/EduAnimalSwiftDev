c68ac4d98e8a2a51f62f8fc004443b74f3cf83ab
Aula Faridah <aulafaridah@gmail.com>
2024-05-02 14:00:40 +0700

    Initial Commit

---
+++ EduAnimalSwiftDev/EduAnimalSwiftDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduAnimalSwiftDevApp.swift
+//  EduAnimalSwiftDev
+//
+//  Created by MacBook Pro on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EduAnimalSwiftDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
